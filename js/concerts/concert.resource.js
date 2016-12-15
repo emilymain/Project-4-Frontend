74e7bcf0abc2c858b4b6e50117f3a9f9afe3f531
Emily Main <emilymuirmain@gmail.com>
2016-12-14 16:19:18 -0800

    updated for github deployment

--- js/concerts/concert.resource.js
+++ js/concerts/concert.resource.js
@@ -6,6 +6,6 @@
   ConcertResource.$inject = ['$resource'];
 
   function ConcertResource($resource) {
-    return $resource('http://localhost:3000/api/concerts/:id', {id: '@_id'}, { 'update': { method: 'PATCH'}})
+    return $resource('https://vast-reaches-90212.herokuapp.com/api/concerts/:id', {id: '@_id'}, { 'update': { method: 'PATCH'}})
   }
 }());
